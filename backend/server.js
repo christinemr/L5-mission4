bf7e9ef23e769a152ae883731cd62d750367d4d9
christine <christinec@missionreadyhq.com>
2025-09-08 11:37:39 +1200

    feat: added simple logic for endpoint, try catch method and error handling.

--- backend/server.js
+++ backend/server.js
@@ -17,23 +17,49 @@ app.use(cors({ origin: "http://localhost:5173" }));
 
 // init Gemini client
 const generativeAI = new GoogleGenerativeAI(API_KEY);
+
+// define Gemini model
 const model = generativeAI.getGenerativeModel({ model: "gemini-2.5-pro" });
 
 // define Tina's persona //
+// TODO: add prompt message under systemInstruction
 async function generateTinaReply(prompt) {
-  const response = await model.generateContent({
-    model: "gemini-2.5-pro",
-    contents: prompt,
+  // init chat session
+  const chat = await model.startChat({
+    history: [],
     config: {
-      systemInstruction: "You are Tina, a playful but helpful cat assistant.",
+      systemInstruction:
+        "You are Tina, a playful but helpful cat assistant. response something not more than 10 sentences",
     },
   });
-  return response.response.text();
+  const result = await chat.sendMessage(prompt);
+  return result.response.text();
 }
 
 // endpoint
-app.post("/recomendation", (req, res) => {
-  res.send("Hello World!");
+app.post("/recommendation", async (req, res) => {
+  // from frontend but update as needed
+  const { prompt } = req.body;
+  console.log("Incoming request body:", req.body);
+
+  if (!prompt) {
+    console.error("⚠️ missing input!");
+    return res.status(400).json({ error: "missing input! Please try again" });
+  }
+
+  try {
+    // generate AI response & reply
+    // sends prompt to Gemini
+    const TinasReply = await generateTinaReply(prompt);
+    console.log({ TinasReply });
+
+    // send res to frontend
+    res.status(200).json({ reply: TinasReply });
+  } catch (error) {
+    // error handling
+    console.error("❌ error generating recommendation ");
+    res.status(400).json({ error: "⚠️ Failed to generate recommendation" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
